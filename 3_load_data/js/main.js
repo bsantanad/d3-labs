095f8b15af0442de08845b5b0208761eca87c325
bsantanad <0211518@up.edu.mx>
2022-06-02 11:30:18 -0500

    added ex 3, with its image
    Signed-off-by: bsantanad <0211518@up.edu.mx>

---
+++ 3_load_data/js/main.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/*
+d3.csv('./data/ages.csv', function(data){
+    console.log(data) 
+});
+
+d3.tsv('./data/ages.tsv', function(data){
+    console.log(data) 
+});
+*/
+
+let ages = [];
+d3.json('data/ages.json').then((data) => {
+    data.forEach((d) => {
+        d.age = +d.age;
+        ages.push(d.age);
+    });
+    draw(ages);
+});
+
+
+function draw(data) {
+    let svg = d3.select('#chart-area').append('svg')
+        .attr('width', 400)
+        .attr('height', 400)
+
+    let circles = svg.selectAll('circle')
+        .data(data);
+
+    circles.enter()
+        .append('circle')
+            .attr('cx', function(d, i) {
+                return (i * 50) + 50;
+            })
+            .attr('cy', 100)
+            .attr('r', function(d) {
+                return d;
+            })
+            .attr('fill', 'blue');
+}
